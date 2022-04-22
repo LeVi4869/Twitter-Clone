69f5ebcefc5228b5e8a64a53e8d5b79bf72bc3fa
LeVi4869 <thanhvinhle3900@gmail.com>
2022-04-22 14:39:22 -0400

    a lil bit more

--- myApp.js
+++ myApp.js
@@ -1,9 +1,12 @@
 //import node stuff
 const http = require('http');
 const displayContent = require('./route');
+const colors = require('colors');
 
 //creates a server
 const server = http.createServer(displayContent);
 
 server.listen(3001);
+
+console.log('hello'.green);
 
