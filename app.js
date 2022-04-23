5b83b2515f422ac6b11b6878cefe92f144145bd5
LeVi4869 <thanhvinhle3900@gmail.com>
2022-04-22 23:36:47 -0400

    login/register stuff in process

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const app = express();
-const port = 3003;
+const port = 4869;
 const middleware = require('./middleware')
+const path = require('path')
+const bodyParser = require("body-parser")
 
 const server = app.listen(port, () => console.log("Server listening on port " + port));
 
 app.set("view engine", "pug");
 app.set("views", "views");
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, "public")));
+
 // Routes
 const loginRoute = require('./routes/loginRoutes');
+const registerRoute = require('./routes/registerRoutes');
 
 app.use("/login", loginRoute);
+app.use("/register", registerRoute);
 
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
