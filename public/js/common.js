5ba0b5585a381e6395f4a7f7a17c152fae0ebb53
LeVi4869 <thanhvinhle3900@gmail.com>
2022-04-29 18:10:27 -0400

    Something about cover photos

--- public/js/common.js
+++ public/js/common.js
@@ -1,3 +1,6 @@
+//globals
+var cropper
+
 $("#postTextarea, #replyTextarea").keyup((event) =>  {
     var textbox = $(event.target);
     var value = textbox.val().trim();
@@ -76,6 +79,91 @@ $('#deletePostButton').click((event) => {
         success: (postData) => {
             location.reload();
         }
+    })
+})
+
+$('#filePhoto').change(function() {
+    if(this.files && this.files[0]) {
+        var reader = new FileReader();
+        reader.onload = (e) => {
+            var image = document.getElementById('imagePreview');
+            image.src = e.target.result
+            if(cropper !== undefined)   {
+                cropper.destroy();
+            }
+            cropper = new Cropper(image, {
+                aspectRatio: 1 / 1,
+                background: false
+            });
+        }
+        reader.readAsDataURL(this.files[0]);
+    }
+})
+
+$('#coverPhoto').change(function() {
+    if(this.files && this.files[0]) {
+        var reader = new FileReader();
+        reader.onload = (e) => {
+            var image = document.getElementById('coverPreview');
+            image.src = e.target.result
+            if(cropper !== undefined)   {
+                cropper.destroy();
+            }
+            cropper = new Cropper(image, {
+                aspectRatio: 16 / 9,
+                background: false
+            });
+        }
+        reader.readAsDataURL(this.files[0]);
+    }
+})
+
+
+$('#imageUploadButton').click(() => {
+    var canvas = cropper.getCroppedCanvas();
+    if(canvas == null)  {
+        alert('Could not upload image. Make sure it is an image file')
+        return;
+    }
+
+    canvas.toBlob((blob) => {
+        var formData = new FormData();
+        formData.append('croppedImage', blob);
+
+        $.ajax({
+            url: '/api/users/profilePicture',
+            type: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: () =>  {
+                location.reload();
+            }
+        })
+    })
+})
+
+$('#coverPhotoButton').click(() => {
+    var canvas = cropper.getCroppedCanvas();
+    if(canvas == null)  {
+        alert('Could not upload image. Make sure it is an image file')
+        return;
+    }
+
+    canvas.toBlob((blob) => {
+        var formData = new FormData();
+        formData.append('croppedImage', blob);
+
+        $.ajax({
+            url: '/api/users/coverPhoto',
+            type: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: () =>  {
+                location.reload();
+            }
+        })
     })
 })
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const upload = multer({ dest: 'uploads/' })
 const User = require('../../schemas/UserSchema')
 const Post = require('../../schemas/PostSchema')
 
@@ -54,6 +58,46 @@ router.get("/:userId/followers", async (req, res, next) => {
     .catch((error) => {
         console.log(error)
         res.sendStatus(400);
+    })
+})
+
+router.post("/profilePicture", upload.single('croppedImage'), async (req, res, next) => {
+    if(!req.file)   {
+        console.log('No file uploaded with ajax request.')
+        return res.sendStatus(400);
+    }
+
+    var filePath = `/uploads/images/${req.file.filename}.png`;
+    var tempPath = req.file.path;
+    var targetPath = path.join(__dirname, `../../${filePath}`);
+    fs.rename(tempPath, targetPath, async (error) => {
+        if(error != null)   {
+            console.log(error);
+            return res.sendStatus(400);
+        }
+        req.session.user = await User.findByIdAndUpdate(req.session.user._id, { profilePic: filePath }, { new: true })
+        
+        res.sendStatus(204);
+    })
+})
+
+router.post("/coverPhoto", upload.single('croppedImage'), async (req, res, next) => {
+    if(!req.file)   {
+        console.log('No file uploaded with ajax request.')
+        return res.sendStatus(400);
+    }
+
+    var filePath = `/uploads/images/${req.file.filename}.png`;
+    var tempPath = req.file.path;
+    var targetPath = path.join(__dirname, `../../${filePath}`);
+    fs.rename(tempPath, targetPath, async (error) => {
+        if(error != null)   {
+            console.log(error);
+            return res.sendStatus(400);
+        }
+        req.session.user = await User.findByIdAndUpdate(req.session.user._id, { coverPhoto: filePath }, { new: true })
+        
+        res.sendStatus(204);
     })
 })
 
