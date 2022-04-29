185ae5b799688746a7a60ff4f1e6899d315fd98f
LeVi4869 <thanhvinhle3900@gmail.com>
2022-04-28 23:54:27 -0400

    followers and following thingy

--- public/js/common.js
+++ public/js/common.js
@@ -139,16 +139,29 @@ $(document).on('click', '.followButton', (event) => {
     $.ajax({
         url: `/api/users/${userId}/follow`,
         type: 'PUT',
-        success: (data) => {
-            console.log(data)
-            // button.find('span').text(postData.retweetUsers.length || '');
+        success: (data, status, xhr) => {
+            if(xhr.status == 404)   {
+                alert('user not found');
+                return;
+            }
 
-            // if(postData.retweetUsers.includes(userLoggedIn._id))    {
-            //     button.addClass('active');
-            // }
-            // else    {
-            //     button.removeClass('active');
-            // }
+            var difference = 1;
+            if(data.following && data.following.includes(userId))    {
+                button.addClass('following');
+                button.text('Following');
+            }
+            else    {
+                button.removeClass('following');
+                button.text('Follow');
+                difference = -1
+            }
+
+            var followersLabel = $('#followersValue')
+            if(followersLabel.length != 0)  {
+                var followersText = followersLabel.text();
+                followersText = parseInt(followersText);
+                followersLabel.text(followersText + difference);
+            }
         }
     })
 })
