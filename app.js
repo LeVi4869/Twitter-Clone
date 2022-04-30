a788c4ddf7f0e72f41024d0074d5bb9c909f7b4e
LeVi4869 <thanhvinhle3900@gmail.com>
2022-04-30 02:05:03 -0400

    search page thingy

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ const logoutRoute = require('./routes/logout');
 const postRoute = require('./routes/postRoutes');
 const profileRoute = require('./routes/profileRoutes');
 const uploadRoute = require('./routes/uploadRoutes');
+const searchRoute = require('./routes/searchRoutes');
 
 //API routes
 const postsApiRoute = require('./routes/api/posts');
@@ -39,6 +40,7 @@ app.use("/logout", logoutRoute);
 app.use('/posts', middleware.requireLogin, postRoute);
 app.use('/profile', middleware.requireLogin, profileRoute);
 app.use('/uploads', uploadRoute);
+app.use('/search', middleware.requireLogin, searchRoute);
 
 app.use("/api/posts", postsApiRoute);
 app.use("/api/users", usersApiRoute);
