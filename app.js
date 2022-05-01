03dce3c3cfded0a16de23d69c3976438612c763e
LeVi4869 <thanhvinhle3900@gmail.com>
2022-05-01 17:08:19 -0400

    chat schema and stuff

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ const messagesRoute = require('./routes/messagesRoutes');
 const postsApiRoute = require('./routes/api/posts');
 const usersApiRoute = require('./routes/api/users');
 const chatsApiRoute = require('./routes/api/chats');
+const messagesApiRoute = require('./routes/api/messages');
  
 app.use("/login", loginRoute);
 app.use("/register", registerRoute);
@@ -48,6 +49,7 @@ app.use('/messages', middleware.requireLogin, messagesRoute);
 app.use("/api/posts", postsApiRoute);
 app.use("/api/users", usersApiRoute);
 app.use("/api/chats", chatsApiRoute);
+app.use("/api/messages", messagesApiRoute);
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
 
