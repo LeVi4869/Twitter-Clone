55e1d58cdced27b7a2d54272a8ce5de7397b5569
LeVi4869 <thanhvinhle3900@gmail.com>
2022-05-01 15:44:13 -0400

    Something about chat names

---
+++ public/js/chatPage.js
@@ -0,0 +1,22 @@
+$(document).ready(() => {
+    $.get(`/api/chats/${chatId}`, data => {
+        $('#chatName').text(getChatName(data))
+    })
+})
+
+$('#chatNameButton').click(() => {
+    var name = $('#chatNameTextbox').val().trim();
+    $.ajax({
+        url: '/api/chats/' + chatId,
+        type: 'PUT',
+        data: { chatName: name },
+        success: (data, status, xhr) => {
+            if(xhr.status != 204)   {
+                alert('Could not update')
+            }
+            else    {
+                location.reload();
+            }
+        }
+    })
+})
