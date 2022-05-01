a8a6f90dc6f8ab7688444b82e3d00782f8915243
LeVi4869 <thanhvinhle3900@gmail.com>
2022-05-01 02:23:13 -0400

    even more chats wow

--- routes/messagesRoutes.js
+++ routes/messagesRoutes.js
@@ -3,7 +3,9 @@ const app = express();
 const router = express.Router();
 const bodyParser = require("body-parser")
 const bcrypt = require('bcrypt')
+const mongoose = require('mongoose')
 const User = require('../schemas/UserSchema')
+const Chat = require('../schemas/ChatSchema')
 
 router.get("/", (req, res, next) => {
     
@@ -26,5 +28,62 @@ router.get("/new", (req, res, next) => {
 
     res.status(200).render("newMessage", payload);
 })
+
+router.get("/:chatId", async (req, res, next) => {
+
+    var userId = req.session.user._id;
+    var chatId = req.params.chatId;
+    var isValidId = mongoose.isValidObjectId(chatId);
+    var payload = {
+        pageTitle: 'Chat',
+        userLoggedIn: req.session.user,
+        userLoggedInJs: JSON.stringify(req.session.user)
+    };
+    if(!isValidId)  {
+        payload.errorMessage = 'Chat does not exist or you do not have permission to view it'
+        return res.status(200).render("chatPage", payload);
+    }
+    var chat = await Chat.findOne({ _id: chatId, users: { $elemMatch: { $eq: userId }}})
+    .populate('users');
+
+    if(chat == null)    {
+        var userFound = await User.findById(chatId)
+        if(userFound != null)   {
+            chat = await getChatByUserId(userFound._id, userId);
+        }
+    }
+    
+    if(chat == null)    {
+        payload.errorMessage = 'Chat does not exist or you do not have permission to view it'
+    }
+    else    {
+        payload.chat = chat;
+    }
+
+    res.status(200).render("chatPage", payload);
+})
+
+function getChatByUserId(userLoggedInId, otherUserId)   {
+    return Chat.findOneAndUpdate({
+        isGroupChat: false,
+        users:  {
+            $size: 2,
+            $all: [
+                { $elemMatch: { $eq: mongoose.Types.ObjectId(userLoggedInId) }},
+                { $elemMatch: { $eq: mongoose.Types.ObjectId(otherUserId) }}
+            ]
+        }
+    },
+    {
+        $setOnInsert:   {
+            users:  [userLoggedInId, otherUserId]
+        }
+    },
+    {
+        new: true,
+        upsert: true
+    })
+    .populate('users');
+}
 
 module.exports = router;
